3f59141a42759dbe30b05208993bc710f985d2cd
Leila Moraes <leila.violino@gmail.com>
2023-06-16 17:31:06 -0300

    feat: implements function exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for i in range(len(instance)):
+        file = instance.search(i)
+        info = {
+            'palavra': word,
+            'arquivo': file['nome_do_arquivo'],
+            'ocorrencias': []
+        }
+        for phrase in file['linhas_do_arquivo']:
+            if word.lower() in phrase.lower():
+                info['ocorrencias'].append({
+                    'linha': file['linhas_do_arquivo'].index(phrase) + 1
+                })
+        if info["ocorrencias"]:
+            result.append(info)
+
+    return result
 
 
 def search_by_word(word, instance):
